4e70d206e7a2f2287e8b98773047cda089982a60
davidlidebrandt <david.lidebrandt@gmail.com>
2021-02-04 18:57:11 +0000

    Add and configure flask app object

--- app.py
+++ app.py
@@ -5,3 +5,21 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo_con = PyMongo(app)
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), port=int(
+        os.environ.get("PORT")), debug=True)
