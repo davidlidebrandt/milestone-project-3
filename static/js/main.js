b8506671bf2d6352eb611aafffea3d5c317f071b
davidlidebrandt <david.lidebrandt@gmail.com>
2021-02-13 06:43:25 +0000

    Add redirect links between login/regiser modals

--- static/js/main.js
+++ static/js/main.js
@@ -5,5 +5,13 @@ $(document).ready(function () {
     $(".loginnav-button").click(function() {
         $("#login").show();
     });
+     $("#redirect-login").click(function() {
+        $("#register").hide();
+        $("#login").show();
+    });
+     $("#redirect-reg").click(function() {
+        $("#login").hide();
+        $("#register").show();
+    });
 
 });
