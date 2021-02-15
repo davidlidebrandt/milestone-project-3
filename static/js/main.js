b3985505cfd3a69063c7df0b70bec1f3c2f5c0ab
davidlidebrandt <david.lidebrandt@gmail.com>
2021-02-15 08:50:00 +0000

    Add closing function for modals

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
     $(".regnav-button").click(function() {
+        $("#login").hide();
         $("#register").show();
+        $(".collapse").removeClass("show");
     });
     $(".loginnav-button").click(function() {
+        $("#register").hide();
         $("#login").show();
+        $(".collapse").removeClass("show");
     });
      $("#redirect-login").click(function() {
         $("#register").hide();
         $("#login").show();
+        $(".collapse").removeClass("show");
     });
      $("#redirect-reg").click(function() {
         $("#login").hide();
         $("#register").show();
+        $(".collapse").removeClass("show");
     });
+     $(".close-btn").click(function() {
+        $("#login").hide();
+        $("#register").hide();
+    });
+
+
 
 });
