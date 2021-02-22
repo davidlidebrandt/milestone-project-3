9e23814c3648e351e6bc965f602a80ac12c0bac5
davidlidebrandt <david.lidebrandt@gmail.com>
2021-02-22 10:03:03 +0000

    Add slider from w3schools article

--- static/js/main.js
+++ static/js/main.js
@@ -42,6 +42,16 @@ $(document).ready(function () {
         checkEmptyRating();
     });
     checkEmptyRating();
+
+    // This part was taken from this w3schools article on how to dynamically display the sliders value https://www.w3schools.com/howto/howto_js_rangeslider.asp
+    let slider = document.getElementById("rating");
+    let output = document.getElementById("display");
+    output.innerHTML = slider.value; 
+
+    
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+    }
 });
 
 
